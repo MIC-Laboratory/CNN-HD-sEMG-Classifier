remove normalization and K-fold for fixing bug

data_preprocessing/ICE_Lab_dataset.py
@@ -1,8 +1,10 @@
 from torch.utils.data import Dataset
 from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
 from utils.ICE_lab_data_preprocessing import ICE_lab_data_preprocessing as utils
 import numpy as np
 import random
+
 class ICE_Lab_dataset(Dataset):
     """
     Initialize the ICE_Lab_dataset class.
@@ -29,9 +31,8 @@ class ICE_Lab_dataset(Dataset):
         Raises:
         AssertionError: If fold_order is greater than or equal to fold.
     """
-    def __init__(self, root,width=8,height=24,train=False,channel=1,fold=2,fold_order=1):
+    def __init__(self, root,width=8,height=24,train=False,channel=1):
         
-        assert fold_order < fold, "fold_order has to be less than fold"
         self.root = root
         self.channel = channel
         self.width = width
@@ -40,29 +41,15 @@ class ICE_Lab_dataset(Dataset):
         self.data, self.label, self.num_classes = root[0],root[1],root[2]
         
         # # Shuffle the data and label indexes
-        data_indexes = self.data.shape[0] #self.data.shape = (1228800, 192)
-        random.seed(42)
-        data_indexes_list = [i for i in range(data_indexes)]
-        random.shuffle(data_indexes_list)
-        self.data = self.data[data_indexes_list]
-        self.label = self.label[data_indexes_list]
+        X_train, X_test, y_train, y_test = train_test_split(self.data, self.label, test_size=0.33, random_state=42)
         if train:
-            self.data = self.data[:int(0.8*self.data.shape[0])]
-            self.label = self.label[:int(0.8*self.label.shape[0])]
+            self.data = X_train
+            self.label = y_train
         else:
-            self.data = self.data[int(0.8*self.data.shape[0]):]
-            self.label = self.label[int(0.8*self.label.shape[0]):]
-        # Split the data and label based on the fold_order
-        # fold_nodes = [i*(data_indexes//(fold+1)) for i in range(fold+1)]
-        # if train:
-        #     # Concatenate the data and label from the specified fold_order
-        #     self.data = np.concatenate(
-        #         (self.data[fold_nodes[0]:fold_nodes[fold_order]],self.data[fold_nodes[fold_order+1]:]))
-        #     self.label = np.concatenate(
-        #         (self.label[fold_nodes[0]:fold_nodes[fold_order]], self.label[fold_nodes[fold_order+1]:]))
-        # else:
-        #     self.data = self.data[fold_nodes[fold_order]:fold_nodes[fold_order+1]]
-        #     self.label = self.label[fold_nodes[fold_order]:fold_nodes[fold_order+1]]
+            self.data = X_test
+            self.label = y_test
+
+        
 
     def __len__(self):
         """
@@ -85,10 +72,8 @@ class ICE_Lab_dataset(Dataset):
         """
         data = self.data[idx]
         label = self.label[idx].astype(int)
-        data = self.utils.NormalizeData(data)
-
-        
         data = data.reshape(self.channel,self.width,self.height)
-
         data = data.astype("float32")
+        
+        
         return data,label

test.py
@@ -1,5 +1,6 @@
 import torch
 import scipy.io
+import numpy as np
 from models.mobilenetv1 import MobilenetV1
 
 classes = {
@@ -13,34 +14,35 @@ classes = {
     7:"N/A"
 }
 # Create an instance of the MobileNetV2 model with specified parameters
-model = MobilenetV1(ch_in=1,n_classes=8,Global_ratio=0.4)
+model = MobilenetV1(ch_in=1,n_classes=8,Global_ratio=1)
 
 # Load the pre-trained model weights
 model.load_state_dict(torch.load(
-    "pretrain_model/MobilenetV1_Param@529.28 k_MAC@903.38 KMac_Acc@96.332.pt"))
+    "pretrain_model/MobilenetV1_Param@3.21 M  _MAC@5.37 MMac_Acc@95.070.pt"))
 
 # Set the model to evaluation mode
 model.eval()
 
 # Load input data from a MATLAB file
-input_data = scipy.io.loadmat("data/Training_Trimmed/001-005-005.mat")['Data']
-
+input_data = scipy.io.loadmat("data/sEMG_Test_Label_hand_close.mat")['Data']
 # Convert the input data to a PyTorch tensor
 input_data = torch.asarray(input_data)
-
+# Remove unusual data
+if input_data.shape[1] == 193:
+    input_data = input_data[:,:-1]
 # Reshape the input data to match the expected shape of the model
 input_data = input_data.reshape(-1, 1, 8, 24)
 
-# Normalize the input data by subtracting the mean and dividing by the standard deviation
-input_data = (input_data - input_data.mean()) / input_data.std()
 
+input_label = 0
+input_label = torch.asarray(input_label)
 # Disable gradient calculation since we're only doing inference
 with torch.no_grad():
     print(f"Number of Input sEMG samples:{input_data.shape[0]}, Labels for those samples are Hand Close")
     # Pass the input data through the model
     outputs = model(input_data)
     # Count the number of occurrences of the most frequent output class
-    output_count = (outputs.topk(1).indices.reshape(-1) == 4).sum(dim=0).item()
+    output_count = (outputs.topk(1).indices.reshape(-1) == input_label).sum(dim=0).item()
     # print(outputs.topk(1).indices.reshape(-1))
     # Get the total number of outputs
     total = input_data.shape[0]

train.py
@@ -50,25 +50,21 @@ testing_dataloaders = []
 
 # Iterate over each fold
 
-for fold in tqdm(range(config["fold"]),desc="Proessing K fold"):
 
-    # Create training and testing datasets for the current fold
+# Create training and testing datasets for the current fold
 
-    training_dataset = dataset(root=root,width=config["input_width"],
-                                    height=config["input_height"],fold=config["fold"],fold_order=fold,channel=config["channel"],train=True)
-    testing_dataset = dataset(root=root,width=config["input_width"],
-                                      height=config["input_height"], fold=config["fold"], fold_order=fold, channel=config["channel"], train=False)
+training_dataset = dataset(root=root,width=config["input_width"],
+                                height=config["input_height"],channel=config["channel"],train=True)
+testing_dataset = dataset(root=root,width=config["input_width"],
+                                    height=config["input_height"], channel=config["channel"], train=False)
 # Create training and testing dataloaders using the datasets
 
-    training_dataloader = DataLoader(
-        training_dataset, batch_size=config["batch_size"], shuffle=True, num_workers=config["num_workers"])
-    testing_dataloader = DataLoader(
-        testing_dataset, batch_size=config["batch_size"], shuffle=False, num_workers=config["num_workers"])
-    
-# Append the dataloaders to the respective lists
+training_dataloader = DataLoader(
+    training_dataset, batch_size=config["batch_size"], shuffle=True, num_workers=config["num_workers"])
+testing_dataloader = DataLoader(
+    testing_dataset, batch_size=config["batch_size"], shuffle=False, num_workers=config["num_workers"])
 
-    training_dataloaders.append(training_dataloader)
-    testing_dataloaders.append(testing_dataloader)
+
 
 # Initialize the MobileNetV1 model
 model = MobilenetV1(ch_in=training_dataset.channel,
@@ -195,6 +191,7 @@ def test(epoch,model,criterion,dataloader):
             for i, data in enumerate(dataloader, 0):
                 # Extract inputs and labels from the current batch
                 inputs, labels = data
+
                 # Prepare inputs and labels for computation on the appropriate device
                 labels = labels.type(torch.LongTensor)
                 inputs, labels = inputs.to(device), labels.to(device)
@@ -220,31 +217,27 @@ def test(epoch,model,criterion,dataloader):
 
 
 epochs = config["Epoch"]
-fold = 1
-train_acces = []
-test_acces = []
-#Iterate over the training and testing dataloaders for each fold
-for training_dataloader,testing_dataloader in zip(training_dataloaders,testing_dataloaders):
-    print(f"fold {fold} start")
-    fold += 1
-    # refresh the model weight
-    model.load_state_dict(model_orginal_weight)
-    for epoch in range(epochs):
-        
-        train_loss,train_acc = train(epoch,model,optimizer,criterion,training_dataloader)
-        test_loss,test_acc = test(epoch,model,criterion,testing_dataloader)
-        
 
-        if not os.path.isdir(config["model_save"]):
-            os.makedirs(config["model_save"])
-        if test_acc > best_acc:
-            best_acc = test_acc
-            torch.save(model.state_dict(),f"{config['model_save']}/MobilenetV1_Param@{params}_MAC@{macs}_Acc@{best_acc:.3f}.pt")
-            
-        scheduler.step()
-        epoch+=1
-    train_acces.append(train_acc)
-    test_acces.append(test_acc)
+#Iterate over the training and testing dataloaders for each fold
+
+
+    # refresh the model weight
+model.load_state_dict(model_orginal_weight)
+for epoch in range(epochs):
+    
+    train_loss,train_acc = train(epoch,model,optimizer,criterion,training_dataloader)
+    test_loss,test_acc = test(epoch,model,criterion,testing_dataloader)
+    
+
+    if not os.path.isdir(config["model_save"]):
+        os.makedirs(config["model_save"])
+    if test_acc > best_acc:
+        best_acc = test_acc
+        torch.save(model.state_dict(),f"{config['model_save']}/MobilenetV1_Param@{params}_MAC@{macs}_Acc@{best_acc:.3f}.pt")
+        
+    scheduler.step()
+    epoch+=1
+
 #Calculate the final average training and testing accuracies
 print(
     f"Final testing Acc:{sum(test_acces)/len(test_acces)} | Final training Acc:{sum(train_acces)/len(train_acces)}")

utils/ICE_lab_data_preprocessing.py
@@ -30,11 +30,14 @@ class ICE_lab_data_preprocessing:
         data = None
         label = None
         num_class = 8
-        for gest in tqdm(range(1,num_class+1),desc="Processing Files"):
-            for i in tqdm(range(1,5),desc="Processing Files"):
+        for gest in tqdm(range(1,num_class + 1,1),desc="Processing Files"):
+            for i in tqdm(range(1,6),desc="Processing Files"):
                 if data is None:
                     data = self.load_from_mat(f"{fileAddress}/001-00{gest}-00{i}.mat")
+                    if data.shape[1] == 193:
+                        data = data[:,:-1]
                     label = np.repeat(gest-1,data.shape[0])
+                    
                 else:
                     temp = self.load_from_mat(f"{fileAddress}/001-00{gest}-00{i}.mat")
                     if temp.shape[1] == 193: